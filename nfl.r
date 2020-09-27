60f1171c7a45d9301c114848c9c52409823665ff
Danhfg <danielhenriquefg@gmail.com>
2020-09-27 10:52:07 -0300

    Create nfl.r

---
+++ nfl.r
@@ -0,0 +1,13 @@
+install.packages("fivethirtyeight")
+
+install.packages("ggplot2")
+
+library(fivethirtyeight)
+
+library(ggplot2)
+
+head(nfl_suspensions)
+
+help(nfl_suspensions)
+
+ggplot(nfl_suspensions)
